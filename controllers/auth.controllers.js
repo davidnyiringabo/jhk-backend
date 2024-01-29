9fd5895505dd3e482a5324fb9a5fdfb770e53de9
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-01-29 15:43:07 +0200

    feat: add husky configuration

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,13 +1,13 @@
 exports.login = (req, res) => {
+  const { email, password } = req.body;
 
-  const {email, password} = req.body;
-
-  if(!email || !password) {
-    res.send({
-      message: "Provide all required credentials!"
-    }).status(401);
-  }else {
-    
+  if (!email || !password) {
+    res
+      .send({
+        message: "Provide all required credentials!",
+      })
+      .status(401);
+  } else {
   }
   console.log("login endpoint called");
 
