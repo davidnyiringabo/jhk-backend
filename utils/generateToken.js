7709b73617a641cc8eca59bd809924e3842d0079
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-01-30 06:03:10 +0200

    feat: add error handling to the jwt generation function

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,26 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (email, id) => {
-    try {
-        if (!process.env.JWT_KEY) {
-            throw new Error("JWT_KEY is not defined in the environment.");
-        }
-
-        const token = jwt.sign(
-            {
-                email: email,
-                id: id,
-                exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 30)
-            },
-            process.env.JWT_KEY
-        );
-
-        console.log(token);
-        return token;
-    } catch (error) {
-        console.error("Error generating token:", error.message);
-        return null;
+  try {
+    if (!process.env.JWT_KEY) {
+      throw new Error("JWT_KEY is not defined in the environment.");
     }
+
+    const token = jwt.sign(
+      {
+        email: email,
+        id: id,
+        exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30,
+      },
+      process.env.JWT_KEY,
+    );
+
+    console.log(token);
+    return token;
+  } catch (error) {
+    console.error("Error generating token:", error.message);
+    return null;
+  }
 };
 
 generateToken("nyiringabodavid62@gmail.com", "lakj12-3ljhf8-lkajsdfh-83839sj");
