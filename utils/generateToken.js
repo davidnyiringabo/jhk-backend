beee33a955bb6bc03a69f66a094fc502ee0d5dec
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-01-30 15:32:46 +0200

    feat: finish the register api

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+require("dotenv").config();
 
 const generateToken = (email, id) => {
   try {
@@ -13,8 +13,8 @@ const generateToken = (email, id) => {
         id: id,
         exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30,
       },
-    //   process.env.JWT_KEY,
-    'jakarabotiboardi'
+      //   process.env.JWT_KEY,
+      "jakarabotiboardi",
     );
     return token;
   } catch (error) {
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -20,7 +20,16 @@ const sendEmail = async (email, code) => {
       from: process.env.EMAIL_NAME,
       to: email,
       subject: "JHK Hospital Account Verification Code",
-      html: `<h1>JHK Hospital Account verification code. </br> <p>This is your verification code ${code}</p></h1>`,
+      html: `
+        <div style="font-family: poppins,'Poppins', sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f7f7f7; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+          <h2 style="color: #333; text-align: center;">JHK Hospital Account Verification</h2>
+          <p style="color: #555; text-align: center;">This is your verification code:</p>
+          <div style="text-align: center; padding: 20px; background-color: #fff; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+            <h1 style="color: #007BFF; font-size: 36px; margin: 0;">${code}</h1>
+          </div>
+          <p style="color: #555; text-align: center; margin-top: 20px;">Use this code to verify your account at JHK Hospital.</p>
+        </div>
+      `,
     };
 
     const info = await transporter.sendMail(mailOptions);
