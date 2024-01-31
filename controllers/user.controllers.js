59210636dd2dca8ea695ca6f73b515de1d9e7539
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-01-31 20:57:00 +0200

    feat: enhance middleware + update user route

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,5 +1,5 @@
 const { client } = require("../config/database/connect");
-const checkUserExistance = require("../utils/exists")
+const checkUserExistance = require("../utils/exists");
 
 exports.updateUser = async (req, res) => {
   const { firstName, lastName, email, position, phone } = req.body;
@@ -16,22 +16,28 @@ exports.updateUser = async (req, res) => {
       .send({ message: "Please Provide all credentials!", status: 400 });
   }
 
-  try{
+  try {
     const updateQuery =
-    "UPDATE users SET firstName = $1, lastName = $2, email = $3, position = $4, phone = $5 WHERE email = $3";
-  const updatedUser = await client.query(updateQuery, [
-    firstName,
-    lastName,
-    email,
-    position,
-    phone,
-  ]);
-  console.log(updatedUser);
+      "UPDATE users SET firstName = $1, lastName = $2, email = $3, position = $4, phone = $5 WHERE email = $3";
+    const updatedUser = await client.query(updateQuery, [
+      firstName,
+      lastName,
+      email,
+      position,
+      phone,
+    ]);
+    console.log(updatedUser);
 
-  res.status(200).send({message: "Updated user successfully!", status: 200});
-  }
-  catch(err){
+    res
+      .status(200)
+      .send({ message: "Updated user successfully!", status: 200 });
+  } catch (err) {
     console.log("There was an error while updating the user: ", err);
-    return res.status(500).send({message: "error occured while updating the user! try again later!", status: 500});
+    return res
+      .status(500)
+      .send({
+        message: "error occured while updating the user! try again later!",
+        status: 500,
+      });
   }
 };
